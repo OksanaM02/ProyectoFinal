d44dd5dd19206cefd903130eb3f91a939ea636f2
OksanaM02 <120195456+OksanaM02@users.noreply.github.com>
2024-03-11 18:40:37 +0100

    swagger

--- PROYECTOFINAL/src/app.js
+++ PROYECTOFINAL/src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { init } from './loaders/index.js';
 import config from './config.js';
+import { swaggerSpec, swaggerUi } from '../swaggerConfig.js'; // Importa swaggerUi correctamente
 
 const app = express();
 
 init(app, config);
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec)); // Usa swaggerUi correctamente
 export default app;
