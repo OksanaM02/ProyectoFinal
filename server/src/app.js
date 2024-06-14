6661e814b328b3a48023f88ebc688c1589cd86ed
OksanaM02 <120195456+OksanaM02@users.noreply.github.com>
2024-06-14 11:54:17 +0200

    cosas

--- server/src/app.js
+++ server/src/app.js
@@ -6,15 +6,19 @@ import YAML from 'yamljs';
 const app = express();
 const port = 3000;
 
+// Define la ruta al archivo swagger.yaml de manera absoluta
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(YAML.load('../swagger.yaml')));
+// Configura Swagger UI para servir la documentación de la API
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
     res.json({ message: 'API Funciona??' });
 });
 
-
+// Inicia el servidor
 app.listen(port, () => {
     console.log(`Servidor listo en http://localhost:${port}`);
 });
