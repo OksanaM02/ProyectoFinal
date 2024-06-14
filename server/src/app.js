3d940e696959d0eb391d83b238c3065c89254fb0
OksanaM02 <120195456+OksanaM02@users.noreply.github.com>
2024-06-14 12:02:52 +0200

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,15 +2,11 @@ import express from 'express';
 import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const swaggerPath = path.join(__dirname, '../swagger.yaml');
 
 const app = express();
 const port = 3000;
 
-const swaggerDocument = YAML.load(swaggerPath);
+const swaggerDocument = YAML.load(path.join(__dirname, '../swagger.yaml'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,6 +16,4 @@ app.get('/', (req, res) => {
     res.json({ message: 'API Funciona??' });
 });
 
-app.listen(port, () => {
-    console.log(`Servidor listo en http://localhost:${port}`);
-});
+export default app;
